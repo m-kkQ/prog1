a6dd3044e3d326b8a80538f00b7459d8cbd3393d
PatrickHome <patr846@gmail.com>
2020-06-26 12:57:51 +0200

    .

--- JavaFX/src/EclipseFX.java
+++ JavaFX/src/EclipseFX.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -6,22 +14,31 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class EclipseFX extends Application {
 
 	private TextArea editor;
+	private TextArea console;
+	private TextArea projectview;
+	private TextArea outline;
 	private Label lblStatus;
+	private Stage mainStage;
+	private File workingFile;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		mainStage = primaryStage;
 
 		BorderPane mainPane = new BorderPane();
 
 		mainPane.setTop(buildNavBar());
 		mainPane.setLeft(buildProjectView());
-		mainPane.setCenter(MainView());
-		mainPane.setBottom(buildStatusPane());
+		mainPane.setCenter(buildEditorPane());
+		mainPane.setRight(buildOutlineView());
+		mainPane.setBottom(BottomView());
 
 		Scene scene = new Scene(mainPane);
 		primaryStage.setScene(scene);
@@ -31,28 +48,16 @@ public class EclipseFX extends Application {
 
 	}
 
-	private Node MainView() {
+	private Node BottomView() {
 
-		BorderPane MainView = new BorderPane();
+		BorderPane BottomView = new BorderPane();
 
-		MainView.setLeft(TopMain());
-		MainView.setBottom(buildConsole());
+		BottomView.setTop(buildConsole());
+		BottomView.setBottom(buildStatusPane());
 
-		return MainView;
+		return BottomView;
 	}
 
-	private Node TopMain() {
-
-		BorderPane TopMain = new BorderPane();
-
-		TopMain.setLeft(buildEditorPane());
-		TopMain.setRight(buildOutlineView());
-
-		return TopMain;
-
-	}
-
-	// Console Panels
 	private Node buildConsole() {
 
 		BorderPane ConsolePane = new BorderPane();
@@ -64,8 +69,8 @@ public class EclipseFX extends Application {
 	}
 
 	private Node ConsoleView() {
-		editor = new TextArea();
-		return editor;
+		console = new TextArea();
+		return console;
 	}
 
 	private Node ConsoleTabs() {
@@ -81,8 +86,6 @@ public class EclipseFX extends Application {
 		return hbox;
 
 	}
-
-	// Editor Pane
 
 	private Node buildEditorPane() {
 
@@ -110,7 +113,6 @@ public class EclipseFX extends Application {
 		return hbox;
 	}
 
-	// Top Bars
 	private Node buildNavBar() {
 
 		BorderPane navPane = new BorderPane();
@@ -135,30 +137,126 @@ public class EclipseFX extends Application {
 
 	private Node NavBar() {
 		HBox hbox = new HBox(2);
+		
+		Button btnNew = new Button("New");
+		btnNew.setOnAction(ev -> {
+			lblStatus.setText("New file");
+			editor.clear();
+			workingFile = null;
+			
+		});
 
-		Button btnOpen = new Button("Öffnen");
-
+		Button btnOpen = new Button("Open");
 		btnOpen.setOnAction(ev -> {
-			lblStatus.setText("Datei öffnen");
+			lblStatus.setText("Open file...");
+			chooseFileAndOpen();
+
 		});
 
-
-		Button btnSave = new Button("Speichern");
+		Button btnSave = new Button("Save as..");
 		btnSave.setOnAction(ev -> {
-			lblStatus.setText("Datei gespeichert");
+			lblStatus.setText("Saved as " + workingFile.getAbsolutePath());
+			chooseFileAndSave();
 		});
 
-	
-		Button btnSaveAs = new Button("Speichern als...");
+		Button btnSaveAs = new Button("Save");
 		btnSaveAs.setOnAction(ev -> {
-			lblStatus.setText("Datei speichern als...");
+			saveAsToFile();
+			lblStatus.setText("Saved");
 		});
 
-
-		hbox.getChildren().addAll(btnOpen, btnSave, btnSaveAs);
+		hbox.getChildren().addAll(btnNew, btnOpen, btnSave, btnSaveAs);
 
 		return hbox;
 
+	}
+
+	private void saveAsToFile() {
+		if (workingFile == null) {
+			chooseFileAndSave();			
+		} else {
+			saveFileFromEditor();
+		}
+		
+	}
+
+	private void chooseFileAndSave() {
+		File selectedFile = chooseFileToSave();
+		
+		if(selectedFile != null) {
+			workingFile = selectedFile;
+			saveFileFromEditor();
+		}
+	}
+
+	private void saveFileFromEditor() {
+			try (BufferedWriter writer =
+					new BufferedWriter(
+							new FileWriter(
+									workingFile))) {
+
+				
+			String text = editor.getText();
+			writer.write(text);
+			lblStatus.setText("File " + workingFile.getAbsolutePath() + " saved");
+			
+			
+			} catch (IOException e) {
+				lblStatus.setText("File not saved");
+			}
+		}
+	
+
+	private void chooseFileAndOpen() {
+		File selectedFile = chooseFileToOpen();
+
+		if(selectedFile != null) {
+			workingFile = selectedFile;
+			openFileAndShow();
+		}
+	}
+
+	private void openFileAndShow() {
+				try (BufferedReader reader =
+						new BufferedReader(
+								new FileReader(
+										workingFile))) {
+
+				editor.clear();
+				String line = "";
+				while ((line = reader.readLine()) != null) {
+					editor.appendText(line);
+					editor.appendText("\n");
+				}
+				lblStatus.setText("Opened file: " + workingFile.getAbsolutePath());
+
+			} catch (FileNotFoundException e) {
+				lblStatus.setText("File not found");
+			} catch (IOException e) {
+				lblStatus.setText("File not read");
+			}
+
+		}
+	
+
+	private File chooseFileToOpen() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open...");
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt", "*.md", "*.rtf"),
+				new ExtensionFilter("Java Files", "*.java"), new ExtensionFilter("Common Lisp File", "*.cl", "*.lisp"));
+
+		File selectedFile = fileChooser.showOpenDialog(mainStage);
+		return selectedFile;
+	}
+
+	private File chooseFileToSave() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Save...");
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt", "*.md", "*.rtf"),
+				new ExtensionFilter("Java Files", "*.java"), new ExtensionFilter("Common Lisp File", "*.cl", "*.lisp"));
+
+		File selectedFile = fileChooser.showSaveDialog(mainStage);
+		return selectedFile;
 	}
 
 	private Node buildOutlineView() {
@@ -186,7 +284,7 @@ public class EclipseFX extends Application {
 		HBox hbox = new HBox();
 		Button btn1 = new Button("Variables");
 		Button btn2 = new Button("Functions");
-		Button btn3 = new Button("Anderer Scheiß");
+		Button btn3 = new Button("Other");
 		hbox.getChildren().addAll(btn1, btn2, btn3);
 		return hbox;
 	}
@@ -202,9 +300,9 @@ public class EclipseFX extends Application {
 	}
 
 	private Node OutlineText() {
-		editor = new TextArea();
+		outline = new TextArea();
 
-		return editor;
+		return outline;
 	}
 
 	private Node buildProjectView() {
@@ -228,15 +326,15 @@ public class EclipseFX extends Application {
 
 	private Node ProjectText() {
 
-		editor = new TextArea();
+		projectview = new TextArea();
 
-		return editor;
+		return projectview;
 
 	}
-	
+
 	private Node buildStatusPane() {
 		HBox hbox = new HBox(10);
-		
+
 		lblStatus = new Label("ok");
 		hbox.getChildren().add(lblStatus);
 		return hbox;
